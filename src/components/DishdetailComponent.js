380b26adaadf7ae2893b9da638b1c5cb02b7cc0c
df-garcia <df.garcia@uniandes.edu.co>
2020-06-29 17:22:41 -0500

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -17,19 +17,21 @@ class Dishdetail extends Component {
   renderDish(dishEntrada) {
     if (dishEntrada != null) {
       return (
-        <Card>
-          <CardImg
-            width="100%"
-            src={dishEntrada.image}
-            alt={dishEntrada.name}
-          />
-          <CardBody>
-            <CardTitle>
-              <h4>{dishEntrada.name}</h4>
-            </CardTitle>
-            <CardText>{dishEntrada.description}</CardText>
-          </CardBody>
-        </Card>
+        <div >
+          <Card>
+            <CardImg
+              width="100%"
+              src={dishEntrada.image}
+              alt={dishEntrada.name}
+            />
+            <CardBody>
+              <CardTitle>
+                <h4>{dishEntrada.name}</h4>
+              </CardTitle>
+              <CardText>{dishEntrada.description}</CardText>
+            </CardBody>
+          </Card>
+        </div>
       );
     } 
     else {
@@ -42,17 +44,21 @@ class Dishdetail extends Component {
       const dishActual = this.props.dish;
       const comments = this.props.dish.comments.map((element) =>{
         return(
-          <div key={element.id} className="list-unstyled">
-            <p>{element.comment}</p>
-            <p>--{element.author}, {element.date}</p>
+          <div key={element.id}>
+            <div className="list-unstyled">
+              <p>{element.comment}</p>
+              <p>--{element.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(element.date)))}</p>
+            </div>
           </div>
         );
       });
 
       return (
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">{this.renderDish(dishActual)}</div>
-          <div className="col-12 col-md-5 m-1"><h4>Comments</h4>{comments}</div>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-5 m-1">{this.renderDish(dishActual)}</div>
+            <div className="col-12 col-md-5 m-1"><h4>Comments</h4>{comments}</div>
+          </div>
         </div>  
       );
     }
